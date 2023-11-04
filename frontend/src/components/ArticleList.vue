274c62c09e95e54185f002b3e0ed8a879c9b75a3
Will <gostjoketw@gmail.com>
2023-11-03 18:19:25 -0700

    2023/11/03

--- frontend/src/components/ArticleList.vue
+++ frontend/src/components/ArticleList.vue
@@ -23,6 +23,21 @@
         <div>{{ formatted_time(article.created) }}</div>
     </div>
 
+    <div id="paginator">
+        <span v-if="is_page_exists('previous')">
+            <router-link :to="{ name: 'Home', query: { page: get_page_param('previous') } }">
+                Prev
+            </router-link>
+        </span>
+        <span class="current-page">
+            {{ get_page_param('current') }}
+        </span>
+        <span v-if="is_page_exists('next')">
+            <router-link :to="{ name: 'Home', query: { page: get_page_param('next') } }">
+                Next
+            </router-link>
+        </span>
+    </div>
 </template>
 
 <script>
@@ -44,6 +59,52 @@
             formatted_time: function (iso_date_string) {
                 const date = new Date(iso_date_string);
                 return date.toLocaleDateString()
+            },
+            is_page_exists(direction) {
+                if (direction === 'next') {
+                    return this.info.next !== null
+                }
+                return this.info.previous !== null
+            },
+            // 获取页码
+            get_page_param: function (direction) {
+                try {
+                    let url_string;
+                    switch (direction) {
+                        case 'next':
+                            url_string = this.info.next;
+                            break;
+                        case 'previous':
+                            url_string = this.info.previous;
+                            break;
+                        default:
+                            return this.$route.query.page
+                    }
+
+                    const url = new URL(url_string);
+                    return url.searchParams.get('page')
+                }
+                catch (err) {
+                    return
+                }
+            },
+            // 获取文章列表数据
+            get_article_data: function () {
+                let url = '/api/article';
+                const page = Number(this.$route.query.page);
+                if (!isNaN(page) && (page !== 0)) {
+                    url = url + '/?page=' + page;
+                }
+
+                axios
+                    .get(url)
+                    .then(response => (this.info = response.data))
+            }
+        },
+        watch: {
+            // 监听路由是否有变化
+            $route() {
+                this.get_article_data()
             }
         }
     }
@@ -72,5 +133,21 @@
         background-color: #4e4e4e;
         color: whitesmoke;
         border-radius: 5px;
+    }
+
+    #paginator {
+        text-align: center;
+        padding-top: 50px;
+    }
+
+    a {
+        color: black;
+    }
+
+    .current-page {
+        font-size: x-large;
+        font-weight: bold;
+        padding-left: 10px;
+        padding-right: 10px;
     }
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,4 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+URLSearchParams.prototype.appendIfExists = function(key, value){
+    if (value !== null && value !== undefined){
+        this.append(key, value)
+    }
+}
+
 createApp(App).use(router).mount('#app')
+
+
